e8a37dfba95e27451019b77225ce5a19cdfe4122
lipton <clone0922@gmail.com>
2024-05-01 16:20:51 +0900

    Feat : Buf.h

--- buf.c
+++ buf.c
@@ -12,24 +12,60 @@ struct lruList      lruListHead;
 
 void BufInit(void)
 {
-
+    for(int i = 0; i < MAX_BUFLIST_NUM; i++){
+        CIRCLEQ_INIT(&bufList[i]);
+    }
+    for(int i = 0; i < MAX_BUF_STATE_NUM; i++){
+        CIRCLEQ_INIT(&stateList[i]);
+    }
+    CIRCLEQ_INIT(&freeListHead);
+    CIRCLEQ_INIT(&lruListHead);
 }
 
 Buf* BufFind(int blkno)
 {
-
+    Buf* tmp;
+    int hash = blkno % MAX_BUFLIST_NUM;
+    CIRCLEQ_FOREACH(tmp, &bufList[hash],  blist){
+        if(tmp-> blkno == blkno){
+            return tmp;
+        }
+    }
+    return NULL;
 }
 
 
 void BufRead(int blkno, char* pData)
 {
+    Buf* tmp;
+    int hash = blkno % MAX_BUFLIST_NUM;
+    CIRCLEQ_FOREACH(tmp, &bufList[hash],  blist){
+        if(tmp-> blkno == blkno){
+            pData = tmp -> pMem;
+            return;
+        }
+    }
+
+    pData = NULL;
 
 }
 
 
 void BufWrite(int blkno, char* pData)
 {
+    Buf* tmp;
+    int hash = blkno % MAX_BUFLIST_NUM;
 
+    CIRCLEQ_FOREACH(tmp, &bufList[hash],  blist){
+        if(tmp-> blkno == blkno){
+            tmp -> pMem = pData;
+            tmp -> state = BUF_STATE_DIRTY;
+            CIRCLEQ_REMOVE(&bufList[hash], tmp, blist);
+            CIRCLEQ_REMOVE(&stateList[BUF_CLEAN_LIST], tmp, blist);
+            
+            return;
+        }
+    }
 }
 
 void BufSync(void)
